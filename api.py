8c150b6a329df59e43fa7cbd58f25e34707546cd
NicholasLe04 <Nicholasl.le224@gmail.com>
2023-06-16 22:17:49 -0700

    epic pr changes #1

--- api.py
+++ api.py
@@ -1,34 +1,69 @@
+import os
+import threading
+import enum
+from time import sleep
+from subprocess import Popen, DEVNULL
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import os, subprocess, threading, enum
+
 from pytube import YouTube
-from time import sleep
+from pytube.exceptions import AgeRestrictedError, VideoUnavailable, RegexMatchError
+
 
 class State(enum.Enum):
     INTERLUDE = "interlude"
     PLAYING = "playing"
 
 app = FastAPI()
-current_state = State.INTERLUDE
-interlude_command = ['ffmpeg', '-re', '-stream_loop', '-1', 
-                     '-i', "./interlude.mp4", '-vf', 
-                     'scale=256:144', '-c:v', 'libx264', 
+interlude_command = ['ffmpeg', '-re', '-stream_loop', '-1', '-i', "./interlude.mp4", '-vf', 
+                     'scale=426:240', '-c:v', 'libx264', 
                      '-preset', 'veryfast', '-tune', 'zerolatency', 
                      '-c:a', 'aac', '-ar', '44100', '-f', 'flv', 
                      'rtmp://localhost:1935/live/mystream']
-interlude_process = subprocess.Popen(interlude_command)
-video_process = None
+process_dict = {}
+process_dict[State.INTERLUDE] = Popen(interlude_command, stdout=DEVNULL, stdin=DEVNULL, stderr=DEVNULL)
+current_video_data = {}
+
+# Ensure video folder exists
+if not os.path.exists("./videos"):
+   os.makedirs("./videos")
 
 # Enable CORS
-origins = ["*"]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+def download_and_play(url:str):
+    # Download video
+    video = YouTube(url)
+    video = video.streams.get_by_resolution("360p")
+    videoToPlay = video.default_filename
+    # Download video is not already downloaded
+    if (videoToPlay not in os.listdir("./videos")):
+        video.download("./videos/")
+        # Wait until video is downloaded
+        while videoToPlay not in os.listdir("./videos"):
+            sleep(0.5)
+    # Stop interlude
+    interlude_process = process_dict.pop(State.INTERLUDE)
+    interlude_process.terminate()
+    interlude_process.wait()
+    # Start streaming video
+    process_dict[State.PLAYING] = Popen(['ffmpeg', '-re', '-i', f"./videos/{videoToPlay}", 
+                                      '-vf', 'scale=426:240', '-c:v', 'libx264', 
+                                      '-preset', 'veryfast', '-tune', 'zerolatency', 
+                                      '-c:a', 'aac', '-ar', '44100', '-f', 'flv', 
+                                      'rtmp://localhost:1935/live/mystream'],  
+                                      stdout=DEVNULL, stdin=DEVNULL, stderr=DEVNULL)
+    process_dict[State.PLAYING].wait()
+    process_dict.pop(State.PLAYING)
+    # Once video is finished playing (or stopped early), restart interlude
+    process_dict[State.INTERLUDE] = Popen(interlude_command,  stdout=DEVNULL, stdin=DEVNULL, stderr=DEVNULL)
 
 @app.get("/")
 async def root():
@@ -36,57 +71,46 @@ async def root():
 
 @app.get("/state/")
 async def state():
-    global current_state
-    return { "state": current_state }
+    if State.PLAYING in process_dict:
+        return { "state": State.PLAYING }
+    else:
+        return { "state": State.INTERLUDE }
+
+@app.get("/current/")
+async def current():
+    return { "title": current_video_data["title"], "thumbnail": current_video_data["thumbnail"] }
 
 @app.post("/play/")
 async def play(url: str):
-    global current_state, interlude_process
     # Check if video is already playing
-    if current_state is State.INTERLUDE:
+    if State.INTERLUDE in process_dict:
         # Start thread to download video, stream it, and provide a response
         try:
-            YouTube(url).bypass_age_gate()
-            threading.Thread(target=downloadAndPlay, args=(url,)).start()
+            video = YouTube(url)
+            # Check for age restriction exception
+            video.bypass_age_gate()
+            threading.Thread(target=download_and_play, args=(url,)).start()
+            current_video_data["title"] = video.title
+            current_video_data["thumbnail"] = video.thumbnail_url 
             return { "detail": "Success" }
-        # If download is unsuccessful, give failed response
-        except:
-            return { "detail": "Failed" }
+        # If download is unsuccessful, give response and reason
+        except AgeRestrictedError:
+            return { "detail": "This video is age restricted :(" }
+        except VideoUnavailable:
+            return { "detail": "This video is unavailable :(" }
+        except RegexMatchError:
+            return { "detail": "That's not a YouTube link buddy ..." }
+        except Exception as e:
+            return { "detail": str(e) }
+    return { "detail": "please wait for the current video to end, then make the request" }
     
 @app.post("/stop/")
 async def stop():
-    global current_state, video_process, interlude_process
     # Check if there is a video playing to stop
-    if current_state is State.PLAYING:
+    if State.PLAYING in process_dict:
         # Stop the video playing subprocess
-        video_process.terminate()
-        video_process.wait()
-        # Restart the interlude subprocess
-        current_state = State.INTERLUDE
-        interlude_process = subprocess.Popen(interlude_command)
-        return { "message": "EXITED" }
+        process_dict[State.PLAYING].terminate()
+        return { "detail": "EXITED" }
+    return { "detail": "no video is playing at the moment" }
+    
 
-def downloadAndPlay(url:str):
-    global video_process, interlude_process, current_state
-    # Download video
-    video = YouTube(url)
-    video = video.streams.get_by_resolution("360p")
-    videoToPlay = video.default_filename
-    video.download("./videos/")
-    # Wait until video is downloaded
-    while videoToPlay not in os.listdir("./videos"):
-        sleep(0.25) 
-    # Stop interlude 
-    interlude_process.terminate()
-    interlude_process.wait()
-    # Start streaming video
-    current_state = State.PLAYING
-    video_process = subprocess.Popen(['ffmpeg', '-re', '-i', f"./videos/{videoToPlay}", 
-                                      '-vf', 'scale=256:144', '-c:v', 'libx264', 
-                                      '-preset', 'veryfast', '-tune', 'zerolatency', 
-                                      '-c:a', 'aac', '-ar', '44100', '-f', 'flv', 
-                                      'rtmp://localhost:1935/live/mystream'])
-    video_process.wait()
-    # Once video is finished playing, restart interlude
-    current_state = State.INTERLUDE
-    interlude_process = subprocess.Popen(interlude_command)
