dabe2f4ea7b96df29b6f72b3d124e8305471f7ca
Edwin Yue <53625790+edwaddle@users.noreply.github.com>
2025-07-28 21:18:18 -0700

    Forces program to wait if there is a current thread playing a video, to ensure the previous thread stops before starting a new one (#68)
    * Forces program to wait if there is a current thread playing a video, to ensure the previous thread stops before starting a new one
    
    cache play button takes 5s to reload, and stream just pauses while playing the new audio. Also there are some errors in the logs that I haven't looked at yet.
    
    * fixed UI some UI changes
    
    Search bar still shows when you press play, and buttons in cache menu no longer take 5 seconds to display submit. There are some issues in line 348 in server.py which I don't quite understand
    
    * some whitespace fixes
    
    * get rid of error statement
    
    The platform works as long as you don't try to spam out videos, or if you try to use it on bad wifi. If you do, it may lead the stream's frames to not update properly, or introduce weird bugs
    
    * removed unncessary changes

--- server.py
+++ server.py
@@ -95,6 +95,11 @@ def create_ffmpeg_stream(
     if video_path is None:
         logging.info("video_path is None. ffmpeg_stream cancelled.")
         return 2
+    
+    if (stop_all_videos()):
+
+        time.sleep(5)
+
     # Create a subprocess to stream the video using FFmpeg
     command = [
         "ffmpeg",
@@ -132,13 +137,14 @@ def create_ffmpeg_stream(
         current_video_dict["title"] = title
         current_video_dict["thumbnail"] = thumbnail
 
+    logging.info(f"process {process.pid} started for {video_type.value} video: {video_path}")
     process_dict[video_type] = process.pid
     MetricsHandler.streams_count.labels(video_type=video_type.value).inc(amount=1)
     MetricsHandler.stream_state.labels(video_type=video_type.value).set(1)
     # the below function returns 0 if the video ended on its own
     # 137, 1
     exit_code = process.wait()
-    logging.info(f"process {process.pid} started for {video_type.value} video: {video_path}")
+    logging.info(f"process {process.pid} exited with code {exit_code}")
 
     MetricsHandler.subprocess_count.labels(
         exit_code=exit_code,
@@ -151,22 +157,25 @@ def create_ffmpeg_stream(
     if (exit_code == 0 or video_type == State.PLAYING) and play_interlude_after and args.interlude:
         interlude_lock.release()
     hls_lock.release()
-    logging.info(f"exiting create_ffmpeg_stream with exit code {exit_code}")
     return exit_code
 
 
 # stop the video by type
+# have stop_video_by_type return true if kill_child_processes was called
+# else have it return false
 def stop_video_by_type(video_type: State):
     if video_type in process_dict:
         kill_child_processes(process_dict[video_type])
         process_dict.pop(video_type)
+        return True
+    return False
 
-
+#stop_all_videos should also return a boolean, from what stop_video_by_type returned
 def stop_all_videos():
-    stop_video_by_type(State.INTERLUDE)
-    stop_video_by_type(State.PLAYING)
+    return stop_video_by_type(State.INTERLUDE) or stop_video_by_type(State.PLAYING)
+    
 
-
+ 
 # terminate a parent process and all its child processes using a specified signal.
 def kill_child_processes(parent_pid, sig=signal.SIGKILL):
     try:
@@ -205,7 +214,7 @@ def download_and_play_video(
     if video_path is None:
         video_cache.add(url)
         video_path = video_cache.find(Cache.get_video_id(url))
-    stop_all_videos()
+
     return create_ffmpeg_stream(
         video_path,
         State.PLAYING,
@@ -316,7 +325,7 @@ def run_hls_stream():
             stdin=subprocess.DEVNULL,
             stderr=subprocess.PIPE,
         )
-        logging.info(f"HLS worker: started FFmpeg PID {proc.pid}")
+        logging.info(f"HLS process started with pid {proc.pid}")
         # start the logging thread (non-blocking)
         threading.Thread(
             target=_monitor_ffmpeg, args=(proc,), daemon=True
@@ -324,7 +333,7 @@ def run_hls_stream():
         # wait until a playback thread ends
         hls_lock.acquire()
         # rotate: kill, clean, loop
-        logging.info("HLS worker: rotate signal, killing FFmpeg & cleaning dir")
+        logging.info(f"hls_lock acquired, stopping pid {proc.pid}")
         kill_child_processes(proc.pid)
         _clean_hls_dir()
 
@@ -354,13 +363,6 @@ async def state():
 
 @app.post("/play/file")
 async def play_file(file_path: str = "cache", title: str = None, thumbnail: str = None):
-
-    # If any video playing, stop it
-    for video_type in State:
-        # Stop the video playing subprocess
-        stop_video_by_type(video_type)
-
-    # Start thread to stream the video and provide a response
     try:
 
         # check if we are going to play all videos or a single video in the cache
