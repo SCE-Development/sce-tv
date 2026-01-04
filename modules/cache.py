c098b09c9fb8b7e90c619c6ef8f08247d921a10e
taaziz1 <151228567+taaziz1@users.noreply.github.com>
2026-01-03 21:49:43 -0800

    update cache file frequently (#92)
    * update cache file more frequently
    
    * update cache file when downsizing

--- modules/cache.py
+++ modules/cache.py
@@ -80,6 +80,8 @@ class Cache:
         self.current_size_bytes += video_info.size_bytes
         MetricsHandler.cache_size.set(len(self.video_id_to_path))
         MetricsHandler.cache_size_bytes.set(self.current_size_bytes)
+        if self.cache_file:
+            self.write_cache()
 
     def find(self, video_id: str):
         if video_id in self.video_id_to_path:
@@ -97,6 +99,8 @@ class Cache:
             removed_video_info = self.video_id_to_path.popitem(last=False)[1]
             self.current_size_bytes -= removed_video_info.size_bytes
             os.remove(removed_video_info.file_path)
+        if self.cache_file:
+            self.write_cache()
 
     def clear(self):
         self._downsize_cache_to_target_bytes(0)
